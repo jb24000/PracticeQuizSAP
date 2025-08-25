4c3c8c25fd0d22816487b383bc40a0ad1de8d9e0
Melvin J Bonner <140230755+jb24000@users.noreply.github.com>
2025-08-25 00:13:47 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'PracticeQuizSAP-v1';
+const urlsToCache = [
+  '/PracticeQuizSAP/',
+  '/PracticeQuizSAP/index.html',
+  '/PracticeQuizSAP/manifest.json',
+  '/PracticeQuizSAP/sap-questionbank.js',
+  '/PracticeQuizSAP/icons/icon-192.png',
+  '/PracticeQuizSAP/icons/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
